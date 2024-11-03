46b9930b11d6994e7c636594e5a43cf65cef6d52
dev-kawser <kawserofficial.dev@gmail.com>
2024-11-03 12:26:42 +0600

    Add: add custom font size on index css and also xutomize for color on tailwind confiq

---
+++ src/layout/Root.jsx
@@ -0,0 +1,11 @@
+
+
+const Root = () => {
+    return (
+        <div>
+            <h2>Hi All</h2>
+        </div>
+    );
+};
+
+export default Root;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // regular colors
+        pink: "#FB2E86",
+        offNavyBlue: "#3F509E",
+        navyBlue: "#151875",
+
+        purple: "#7E33E0",
+        offPurple: "#9F63B5",
+        pantonePurple: "#E0D3F5",
+
+        red: "#FB2448",
+        blue: "#2F1AC4",
+
+        // dark mode colors
+        darkPink: "#D5005D",
+        darkOffNavyBlue: "#2C3A6A",
+        darkNavyBlue: "#0A0F3B",
+
+        darkPurple: "#5C25B5",
+        darkOffPurple: "#7A4F91",
+        darkPantonePurple: "#B2A4D7",
+
+        darkRed: "#C60D31",
+        darkBlue: "#1E158D",
+      },
+
+    },
   },
   plugins: [],
 }
