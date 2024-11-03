3a86f6113dbd42fc65fee499ebb8fd47e5f552de
dev-kawser <kawserofficial.dev@gmail.com>
2024-11-03 23:46:41 +0600

    Add: add a message for error page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
-        // errorElement: <ErrorPage />,
+        errorElement: <h1>Still Working</h1>,
         children: [
             {
                 path: "/",
